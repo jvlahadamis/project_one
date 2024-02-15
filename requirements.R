b273550572a5e23c43e641cd5731c01d29054403
simon31fu <102628310+simon31fu@users.noreply.github.com>
2024-02-14 19:10:17 -0700

    added lubridate

--- requirements.R
+++ requirements.R
@@ -1,7 +1,7 @@
 
 
 
-p <- c("shiny", "tidyverse", "mongolite", "rvest", "geosphere", "DT")
+p <- c("shiny", "tidyverse", "mongolite", "rvest", "geosphere", "DT", "lubridate")
 new.packages <- p[!(p %in% installed.packages()[, "Package"])]
 if (length(new.packages)) {
   install.packages(new.packages, dependencies = TRUE)
